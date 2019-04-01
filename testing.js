9a7d642e978d88ec2ae98ee6b9080ddd40d16d7b
Julia Álvarez <alvarezjulia1@yahoo.es>
2019-04-01 19:28:24 +0200

    Playing around with rotate and translate

--- testing.js
+++ testing.js
@@ -1,20 +1,24 @@
 function setup() {
     createCanvas(640, 640)
     background(240)
+    angleMode(DEGREES)
 }
 
-let rect1y = 30
-let rect1x = 30
+let rect1y = 300
+let rect1x = 300
 let rect2x = 200
 let rect2y = 120
-let move = 5
+let move = 0.1
+let angle = 1
 
 function draw() {
     clear()
     background(230)
 
     fill(0)
-    rect(rect1x, rect1y, 55, 55, 0, 40, 65, 50)
+    fill('rgba(240,243,46, 0.80)')
+    rotate(angle)
+    triangle(rect1x, rect1y, rect1x + 50, rect1y + 80, rect1x + 30, rect1y + 85)
 
     if (rect1x >= 600) {
         rect1x = 590
@@ -26,28 +30,70 @@ function draw() {
         rect1x = 30
     }
 
-    if (rect1x > rect2x - 60 && rect1x < rect2x + 35 && rect1y > rect2y - 60 && rect1y < rect2y + 85) {
-        move = move * -1.5
-    } else {
-        move = 5
-    }
+    //COLLISION RECTANGLE
+    // if (rect1x > rect2x - 60 && rect1x < rect2x + 35 && rect1y > rect2y - 60 && rect1y < rect2y + 85) {
+    //     move = move * -1.5
+    // } else {
+    //     move = 5
+    // }
 
     if (keyIsDown(LEFT_ARROW)) {
         rect1x -= move
+        translate(50, 50)
+        stroke(0)
+        rotate(angle * 0.05)
+        line(0, 0, 50, 50)
     }
 
     if (keyIsDown(RIGHT_ARROW)) {
         rect1x += move
+        translate(rect1x + 40, rect1y + 40)
+        stroke(0)
+        rotate(angle--)
+        line(0, 0, 50, 50)
     }
 
     if (keyIsDown(UP_ARROW)) {
         rect1y -= move
+        translate(50, 50)
+        stroke(0)
+        rotate(angle * 0.05)
+        line(0, 0, 50, 50)
     }
 
     if (keyIsDown(DOWN_ARROW)) {
         rect1y += move
+        translate(50, 50)
+        stroke(0)
+        rotate(angle * -0.05)
+        line(0, 0, 50, 50)
     }
 
-    fill(50, 140, 20)
-    rect(rect2x, rect2y, 30, 80)
+    // fill(50, 140, 20)
+    // rect(rect2x, rect2y, 30, 80)
 }
+
+//THE CODING TRAIN
+// let angle = 0
+// let x = 50
+// let y = 50
+
+// function setup() {
+//     createCanvas(400, 400)
+//     angleMode(DEGREES)
+// }
+// function draw() {
+//     background(0)
+
+//     translate(x, y)
+//     rotate(angle)
+
+//     stroke(255)
+//     line(0, 0, 50, 50)
+
+//     fill(255)
+//     rectMode(CENTER)
+//     rect(0, 0, 100, 50)
+
+//     angle = angle + 5
+// }
